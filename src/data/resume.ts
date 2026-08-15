0c0b2f6e9daf72f491f6f5dff485cfe3d432858e
nancy-kataria <katarianancy8@gmail.com>
2026-08-14 22:00:31 -0700

    resume link update

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -7,7 +7,7 @@ export const profile = {
   github: "https://github.com/nancy-kataria",
   linkedin: "https://www.linkedin.com/in/nancy-kataria8/",
   resume:
-    "https://drive.google.com/file/d/1Q0vGdIT4WfFHrEr1EThmKEXwJUsgGZFL/view?usp=sharing",
+    "https://drive.google.com/file/d/1WfAZUAtuNtoC38qljunO9-lpnNkBz58h/view?usp=sharing",
 };
 
 export const projects = [
